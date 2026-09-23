5c2bf9e490f297a9cd58a39e4dd30416fda03074
Yosef <mercytv932@gmail.com>
2026-09-22 22:41:22 -0400

    Finished the movie finder api

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -14,6 +14,7 @@ const searchMovies = async (req, res) => {
 
     return res.json(response.data);
   } catch (error) {
+    console.log(error.message);
     return res.status(500).json({ error: "Failed to search movies" });
   }
 };
